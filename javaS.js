6b48ceb1bc298ff286c2ec4c33c4796b55d3b906
Amarah Abusohyon <amarhbishtawi@gmail.com>
2021-03-17 09:20:32 +0200

    finished with detail's local stroage

--- javaS.js
+++ javaS.js
@@ -4,28 +4,33 @@ const image_name_Ext =[['bag','jpg'],['banana','jpg'],['bathroom','jpg'],['boots
   ['chair','jpg'],['cthulhu','jpg'],['dog-duck','jpg'],['dragon','jpg'],['pen','jpg'],['pet-sweep','jpg'],['scissors','jpg'],
   ['shark','jpg'],['sweep','png'],['tauntaun','jpg'],['unicorn','jpg'],['usb','gif'],['water-can','jpg'],['wine-glass','jpg']];
 
-let number_of_section=[]; //// for creating element's
+let number_of_section=[]; //// for creating img's tag , and each img need ID ,;)
 
-let pargh=document.getElementById('paragraph_counter');
+let pargh=document.getElementById('paragraph_counter');////// p tag for counter place in the screen
 
 for(let i=0;i<3;i++){
   number_of_section[i]=document.createElement('img');
-  document.body.appendChild(number_of_section[i]);
-  number_of_section[i].setAttribute('id',`imgID${i}`);
+  document.body.appendChild(number_of_section[i]);/////the img creation depend on the whole on js just no need to html page 
+  number_of_section[i].setAttribute('id',`imgID${i}`);/////the Id for the img
 }
 
-function Product(name,imgExt,votes){
+function Product(name,imgExt,votes){ ////// constructor
   this.name=name;
   this.views=0;
-  this.votes=votes;
+  this.votes=votes; //////for localStorage, by defult the value of it zero ,, look below {to the for loop}
   this.path=`./assets/${name}.${imgExt}`;//it will take the name of url from the array which we made before depend on the name in it's own url
   Product.all.push(this); ///instead of put one by one , using this do all process ..... cute hmm..
 }
 Product.all=[];
+
 for(let i=0;i<image_name_Ext.length;i++){
   let votes=0;
-  if(localStorage.getItem('Product')!==null){ votes=JSON.parse(localStorage.getItem('Product'))[i].votes; console.log('not null');}
-  new Product(image_name_Ext[i][0],image_name_Ext[i][1] , votes);
+  if(localStorage.getItem('Product')!==null){
+    votes=JSON.parse(localStorage.getItem('Product'))[i].votes; //console.log('not null');///json.parse ,retrevie object , the object consist of [array's] for each img depened on {the constructor function},
+    //and each array have number of proparity{name,views,votes,path} so , just take the the votes of specific array /constructor in sequential way ^-^
+  }
+
+  new Product(image_name_Ext[i][0],image_name_Ext[i][1] , votes);////each i (array) have it's own votes to refer
 }
 
 function randomNumber(min, max) {
@@ -34,7 +39,7 @@ function randomNumber(min, max) {
 
 let uniqueIndexArray=[];
 let iteration=0;
-function render(){////////////////////////////////for creating img's with hint name in web page
+function render(){////////////////////////////////for creating img's with hint name in web page and show img's ^-^ for course we need the src
   for(let i=0;i<3;i++){
     const index=randomNumber(0,Product.all.length-1);
     let uniqueIndex=unique(index);
@@ -43,7 +48,7 @@ function render(){////////////////////////////////for creating img's with hint n
   }
   iteration++;
   if(iteration>=2)
-  {uniqueIndexArray.splice(0,3);
+  {uniqueIndexArray.splice(0,3); ////for prevent duplication, in this way , splice will remove 3 element's starting from the index 0
   //  console.log(uniqueIndexArray);
   }
 }
@@ -57,31 +62,69 @@ function unique(indx){
   return indx;
 }
 /////////////////////////////////////////////////////////////////////// local Storage , Function for saving Votes
-//let i=localStorage.getItem('Product');
 function localStorageProduct(){
   let localProduct=JSON.stringify(Product.all);
   localStorage.setItem('Product',localProduct);
 }
+////////////////////////////////////////////////////////////////////////////the Result Section
+let counter=0;/// for count how many iteration(Round), here we want 25 ,and it could be changable
+function showData(event){
+  for(let j=0 ;j<3;j++){
+    if(event.target.id===number_of_section[j].id){
+      //console.log(number_of_section[j].id); if want to see which event we took, for assurance
+      for(let i=0;i<Product.all.length;i++){
+        if (Product.all[i].name === event.target.title){////will count votes and view's for each selected img ,,,, and if we use number_of_section[].title here instead of event.target.id , it's okay^-^
+          Product.all[i].votes++;
+          Product.all[i].views++;
+        }
+      }
+    }
+    else {
+      for(let i=0;i<Product.all.length;i++)////for unselected img's and it appeare on the page it must br countable $-$,
+        if (Product.all[i].name === number_of_section[j].title){/////we use event.target.title in previous section , cuz it the selected ,,, here the ignoring pic :(, there is loop so that it will visit all img's ;)
+          Product.all[i].views++;
+        }
+    }
+    localStorageProduct();
 
-function createChart(){///////////////////////////////////////for chartJS
+  }
+  counter++;
+  render();
+  pargh.innerHTML=counter;////for present counter in web page
+  if(counter===25){
+    for(let i=0; i<3;i++){
+      number_of_section[i].removeEventListener('click',showData); } ////this will stop img's from change and stay stable
+    createChart();
+  }
+
+}
+for(let i=0; i<3;i++){
+  number_of_section[i].addEventListener('click',showData);
+}
+///////////////////////////////////////////////////////////////////////////////////////////////render for seeing the first iteration of 3 Pic's
+render(); ////calling the 3 img's for the first time , and the game will start after that -_- ,the starter point for the rest
+
+//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////chart js
+function createChart(){
   let context = document.getElementById('myChart').getContext('2d');
-  let productViews=[];
+  let productViews=[]; /// creating array's for saving data in each iteration so that present it at the final in the chart
   let productVotes=[];
   let productNames=[];
 
   for(let i=0;i<Product.all.length;i++){
     productNames.push(Product.all[i].name);
     productViews.push(Product.all[i].views);
-    productVotes.push(Product.all[i].votes);
+    productVotes.push(Product.all[i].votes); ////becuase we use local storage the votes will be increment depend on the previous votes
   }
 
-  //////////////////////////////////////////////////////////////Creating Chart using ChartJS Amazing Part
+
   let chartObject ={
     type: 'bar',
     data: {
       labels:productNames,
       datasets: [{//////////////////////////have two type of dataset's one for Voting and other for View's , if u want to add anything to additional just use array for example multiColor's
-        label: 'Product Voting results',
+        label: 'Product Voting results depend on custmor opinion in previous day\'s',
         backgroundColor: [
           'rgba(255, 99, 132, 0.4)',
           'rgba(54, 162, 235, 0.4)',
@@ -108,7 +151,7 @@ function createChart(){///////////////////////////////////////for chartJS
         data:productVotes
       }
       ,{
-        label: 'Product Views results',
+        label: 'Product Views results in this moment',
         backgroundColor: [
           'rgba(255, 99, 132, 0.6)',
           'rgba(54, 162, 235, 0.6)',
@@ -159,42 +202,4 @@ function createChart(){///////////////////////////////////////for chartJS
   };
   let chart = new Chart(context,chartObject);
 }
-////////////////////////////////////////////////////////////////////////////the Result Section
-let counter=0;
-function showData(event){
-  for(let j=0 ;j<3;j++){
-    if(event.target.id===number_of_section[j].id){
-      //console.log(number_of_section[j].id);
-      for(let i=0;i<Product.all.length;i++){
-        if (Product.all[i].name === event.target.title){
-          Product.all[i].votes++;
-          Product.all[i].views++;
-        }
-      }
-    }
-    else {
-      for(let i=0;i<Product.all.length;i++)
-        if (Product.all[i].name === number_of_section[j].title){
-          Product.all[i].views++;
-        }
-    }
-    localStorageProduct();
-
-  }
-  counter++;
-  render();
-  pargh.innerHTML=counter;
-  if(counter===25){
-    for(let i=0; i<3;i++){
-      number_of_section[i].removeEventListener('click',showData); }
-    createChart();
-  }
-
-}
-for(let i=0; i<3;i++){
-  number_of_section[i].addEventListener('click',showData);
-}
-///////////////////////////////////////////////////////////////////////////////////////////////render for seeing the first iteration of 3 Pic's
-render();
-
 
